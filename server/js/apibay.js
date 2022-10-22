4f26ab9c157fe3c395f7e452120a97bc8cb8d8d8
Rafael Marçalo <raroma09@gmail.com>
2022-10-22 19:14:40 +0100

    Replaced axios with stock nodejs https library

--- server/js/apibay.js
+++ server/js/apibay.js
@@ -1,16 +1,26 @@
 "use strict";
 
-const axios = require('axios');
+const https = require('https');
+
 const server = 'https://apibay.org';
 
 async function getRequest(url)
 {
-	return await axios.get(url);
+	let promise = new Promise( resolve => {
+		let data = '';
+
+		https.get(url, res => {
+			res.on('data', chunk => {data += chunk});
+			res.on('end', () => resolve(data));
+		});
+	});
+
+	return await promise;
 }
 
 async function hit(query)
 {
-	return await getRequest(server + '/q.php?q=' + encodeURI(query));
+	return JSON.parse(await getRequest(server + '/q.php?q=' + encodeURI(query)));
 }
 
 module.exports = {hit};
