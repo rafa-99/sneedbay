7ff1d8299574dd142ea38091e28ad5c2941b2427
Rafael Marçalo <raroma09@gmail.com>
2022-10-23 02:25:00 +0100

    Fixed up paths for electron

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 "use strict";
 
 // handling imports
-const express = require('express');
 const bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = module.exports.PORT = process.env.PORT || 3000;
 
-
 // set the public folder to public acess and added a body parser
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, '/public')));
 app.use(bodyParser.urlencoded({extended: true}));
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
 
 // routes management
 const home = require("./routes/home");
